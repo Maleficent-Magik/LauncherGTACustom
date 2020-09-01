3fb1fed6dc7afb1353ad8486423b37eab5702c85
UsedDiscord <57113628+UsedDiscord@users.noreply.github.com>
2020-09-01 15:29:10 +0200

    Add files via upload

--- OnlineModdedGTAV.bat
+++ OnlineModdedGTAV.bat
@@ -1,10 +1,32 @@
 @echo off
-cls
+
 color 5F
+
+net.exe session 1>NUL 2>NUL || goto :not_admin
+
+echo SUCCESS PERMS ADMIN OK. WAIT 2 SECONDS.
+
+timeout 2
+
+goto :startup
+
+:not_admin
+
+echo ERROR: Please run as a local administrator.
+
+timeout 10
+
+exit /b 1
+
+title MODDED OR ONLINE GTAV PROD 0.1 GalaxyStars 
+
+:startup
+
+cls
 
 echo.
 echo       +------------------------------------+
-echo       ^|    MODIFIER ONLINE / MODDED GTA V  ^|
+echo       #    MODIFIER ONLINE / MODDED GTA V  #
 echo       +------------------------------------+
 
 echo .
@@ -13,6 +35,7 @@ echo .
 
 echo       Hello and welcome to my software allowing to switch between GTA Mod - GTA Online!
 echo		 This software will be useful to you, if you have to partition your disc in half for example!
+
 
 :question
 set /p choix= NUM 1 : MODDED GTA  NUM 2 ONLINE(1/2)? :
@@ -28,11 +51,11 @@ cls
 
 color 85
 
-
-echo ooYoYo. .oPYo.     8     8 .oPYo.     8
-echo 8' 8  8 8    8 '8888 '8888 8oooo8 '888'
-echo 8  8  8 8    8 8   8 8   8 8.     8   8
-echo 8  8  8 'YooP' '888' '888' 'Yooo' '888'
+echo ## ##  ###  ###   ###   ##### ###    ########  ####### ## ## #####
+echo # # # #   # #  #  #  #  #     #  #   #         #     # # # # #
+echo #   # #   # #   # #   # ##### #   #  #   ##### ####### #   # #####
+echo #   # #   # #  #  #  #  #     #  #   #       # #     # #   # #
+echo #   #  ###  ###   ###   ##### ###    ######### #     # #   # #####
 
 echo.
 echo.
@@ -49,6 +72,10 @@ echo ####### ###### ###### #      # #     #    #        #    #  ####### ####### 
 
 taskkill /F /IM EpicGamesLauncher.exe
 
+timeout /t 2
+
+cls
+
 echo ######## #######  ######### #        ######## ######## ######   #       ###### ######## ####### ####### #########  
 echo #     #  #        #       # #        #      # #        #        #       #         #        #    #       #      #
 echo #    #   #        #       # #        #      # #        #        #       #         #        #    #       #     #
@@ -63,7 +90,7 @@ echo .
 echo .
 echo .
 
-echo ######  ##      # #      ####### ##      # #######  #########     ###   ###  #######  #####     ######    ###### ######
+echo  ######  ##      # #      ####### ##      # #######  #########     ###   ###  #######  #####     ######    ###### ######
 echo #      # # #     # #         #    # #     # #         ##########   #  ###  # #       # #    #    #     #   #      #     #
 echo #      # #  #    # #         #    #  #    # #            ######### #       # #       # #     #   #      #  #      #      #
 echo #      # #   #   # #         #    #   #   # #######       ######## #       # #       # #      #  #       # #####  #       #
